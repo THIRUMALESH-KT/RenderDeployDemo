1508d695acd00c1e85e087931b4244d4ef09e42d
THIRUMALESH-KT <tthirumal24@gmail.com>
2023-11-28 17:58:28 +0530

    update

--- AWSSpringbootDemo/src/main/java/com/aws/controller/controller.java
+++ AWSSpringbootDemo/src/main/java/com/aws/controller/controller.java
@@ -7,6 +7,7 @@ import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,14 +22,16 @@ public class controller {
 
 	@Autowired
 	private EmployeeRepo repo;
-	@GetMapping("/welcome")
-	public ResponseEntity<Map<String,Object>> welcome(){
-		Map<String , Object> map=new LinkedHashMap<>();
-		map.put("result", "sucess");
-		map.put("body", "This is welcom page ");
-		map.put("status ",HttpStatus.OK);
-		return ResponseEntity.ok(map);
-	}
+	   @GetMapping(value = "/welcome", produces = "application/json")
+	    public ResponseEntity<Map<String, Object>> welcome() {
+	        Map<String, Object> map = new LinkedHashMap<>();
+
+	        map.put("result", "success");
+	        map.put("body", "This is welcome pagee");
+	        map.put("status", HttpStatus.OK);
+
+	        return ResponseEntity.ok(map);
+	    }
 	
 	@GetMapping("/insert")
 	public ResponseEntity<Map<String , Object>> insert(@RequestBody Employee ob){
